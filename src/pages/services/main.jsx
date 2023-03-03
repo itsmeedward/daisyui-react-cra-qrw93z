011513e84412754a466c9020a28387e5b1746c77
itsmeedward <86776978+itsmeedward@users.noreply.github.com>
2023-03-03 06:05:18 +0100

    1 file modified

--- src/pages/services/main.jsx
+++ src/pages/services/main.jsx
@@ -53,7 +53,7 @@ export default function Services() {
   <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
     {/* Title */}
     <div className="max-w-2xl mx-auto text-center mb-10 lg:mb-14">
-      <h2 className="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white">
+      <h2 className="text-2xl font-bold md:text-4xl md:leading-tight ">
         Insights
       </h2>
       <p className="mt-1 text-gray-600 dark:text-gray-400">
