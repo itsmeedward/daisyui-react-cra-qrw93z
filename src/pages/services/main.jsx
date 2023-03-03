d4a7e6990bbd4dfc01bd165c88151e68a650b70c
itsmeedward <86776978+itsmeedward@users.noreply.github.com>
2023-03-03 02:20:44 +0100

    1 file modified

--- src/pages/services/main.jsx
+++ src/pages/services/main.jsx
@@ -6,96 +6,20 @@ export default function Services() {
   {/* ========== MAIN CONTENT ========== */}
   <main id="content" role="main">
     {/* Nav */}
-    <nav
-      className="sticky -top-px bg-white text-sm font-medium text-black ring-1 ring-gray-900 ring-opacity-5 border-t shadow-sm shadow-gray-100 pt-6 md:pb-6 -mt-px dark:bg-slate-900 dark:border-gray-800 dark:shadow-slate-700/[.7]"
-      aria-label="Jump links"
-    >
-      <div className="max-w-7xl snap-x w-full flex items-center overflow-x-auto scrollbar-x px-4 sm:px-6 lg:px-8 pb-4 md:pb-0 mx-auto dark:scrollbar-x">
-        <div className="snap-center shrink-0 pr-5 sm:pr-8 sm:last-pr-0">
-          <a
-            className="inline-flex items-center gap-x-2 hover:text-gray-500 dark:text-gray-400 dark:hover:text-gray-500"
-            href="#"
-          >
-            Dashboard
-          </a>
-        </div>
-        <div className="snap-center shrink-0 pr-5 sm:pr-8 sm:last:pr-0">
-          <a
-            className="inline-flex items-center gap-x-2 hover:text-gray-500 dark:text-gray-400 dark:hover:text-gray-500"
-            href="#"
-          >
-            Users
-          </a>
-        </div>
-        <div className="snap-center shrink-0 pr-5 sm:pr-8 sm:last:pr-0">
-          <a
-            className="inline-flex items-center gap-x-2 hover:text-gray-500 dark:text-gray-400 dark:hover:text-gray-500"
-            href="#"
-          >
-            Account
-          </a>
-        </div>
-        <div className="snap-center shrink-0 pr-5 sm:pr-8 sm:last:pr-0">
-          <a
-            className="inline-flex items-center gap-x-2 hover:text-gray-500 dark:text-gray-400 dark:hover:text-gray-500"
-            href="#"
-          >
-            Projects
-          </a>
-        </div>
-        <div className="snap-center shrink-0 pr-5 sm:pr-8 sm:last:pr-0">
-          <a
-            className="inline-flex items-center gap-x-2 hover:text-gray-500 dark:text-gray-400 dark:hover:text-gray-500"
-            href="#"
-          >
-            Calendar
-          </a>
-        </div>
-        <div className="snap-center shrink-0 pr-5 sm:pr-8 sm:last:pr-0">
-          <a
-            className="inline-flex items-center gap-x-2 hover:text-gray-500 dark:text-gray-400 dark:hover:text-gray-500"
-            href="#"
-          >
-            Documentation
-            <span className="inline bg-gray-100 text-xs text-gray-500 font-semibold rounded-full py-1 px-2 dark:bg-gray-700 dark:text-gray-400">
-              v12.7
-            </span>
-          </a>
-        </div>
-      </div>
-    </nav>
-    {/* End Nav */}
+    
     <div className="max-w-7xl mx-auto py-10 px-4 sm:px-6 lg:px-8 min-h-[75rem]">
       {/* Page Heading */}
       <header className="max-w-3xl">
         <p className="mb-2 text-sm font-semibold text-blue-600">
-          Starter Pages &amp; Examples
+          Services Pages | Our Services
         </p>
-        <h1 className="block text-2xl font-bold text-gray-800 sm:text-3xl dark:text-white">
-          Application Layout: Header using Tailwind CSS
-        </h1>
+        <h1 className="block text-2xl font-bold text-gray-400 sm:text-3xl ">
+        Unlock Your Business's Full Potential with Our Comprehensive Suite of Services!        </h1>
         <p className="mt-2 text-lg text-gray-800 dark:text-gray-400">
-          This is a simple application layout with header only example using
-          Tailwind CSS.
+        From software development to IT consulting, we offer a comprehensive range of services to help businesses succeed. Discover how we can help you today
         </p>
         <div className="mt-5 flex flex-col items-center gap-2 sm:flex-row sm:gap-3">
-          <a
-            className="w-full sm:w-auto inline-flex justify-center items-center gap-x-3 text-center bg-blue-600 hover:bg-blue-700 border border-transparent text-white text-sm font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-white transition py-3 px-4 dark:focus:ring-offset-gray-800"
-            href="https://github.com/htmlstreamofficial/preline/tree/main/examples/html"
-            target="_blank"
-          >
-            <svg
-              className="w-4 h-4"
-              xmlns="http://www.w3.org/2000/svg"
-              width={16}
-              height={16}
-              fill="currentColor"
-              viewBox="0 0 16 16"
-            >
-              <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
-            </svg>
-            Get the source code
-          </a>
+       
           <a
             className="w-full sm:w-auto inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold text-blue-500 hover:text-blue-700 focus:outline-none focus:ring-2 ring-offset-white focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm py-3 px-4 dark:ring-offset-slate-900"
             href="../examples.html"
@@ -119,48 +43,267 @@ export default function Services() {
         </div>
       </header>
       {/* End Page Heading */}
-    </div>
-  </main>
-  {/* ========== END MAIN CONTENT ========== */}
-  {/* ========== SECONDARY CONTENT ========== */}
-  <div
-    id="hs-offcanvas-right"
-    className="hs-offcanvas hs-offcanvas-open:translate-x-0 translate-x-full fixed top-0 right-0 transition-all duration-300 transform h-full max-w-xs w-full w-full z-[60] bg-white border-l dark:bg-gray-800 dark:border-gray-700"
-    tabIndex={-1}
-  >
-    <div className="flex justify-between items-center py-3 px-4 border-b dark:border-gray-700">
-      <h3 className="font-bold text-gray-800 dark:text-white">
-        Offcanvas title
-      </h3>
-      <button
-        type="button"
-        className="hs-dropdown-toggle inline-flex flex-shrink-0 justify-center items-center h-8 w-8 rounded-md text-gray-500 hover:text-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-white text-sm dark:text-gray-500 dark:hover:text-gray-400 dark:focus:ring-gray-700 dark:focus:ring-offset-gray-800"
-        data-hs-offcanvas="#hs-offcanvas-right"
-      >
-        <span className="sr-only">Close modal</span>
-        <svg
-          className="w-3.5 h-3.5"
-          width={8}
-          height={8}
-          viewBox="0 0 8 8"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M0.258206 1.00652C0.351976 0.912791 0.479126 0.860131 0.611706 0.860131C0.744296 0.860131 0.871447 0.912791 0.965207 1.00652L3.61171 3.65302L6.25822 1.00652C6.30432 0.958771 6.35952 0.920671 6.42052 0.894471C6.48152 0.868271 6.54712 0.854471 6.61352 0.853901C6.67992 0.853321 6.74572 0.865971 6.80722 0.891111C6.86862 0.916251 6.92442 0.953381 6.97142 1.00032C7.01832 1.04727 7.05552 1.1031 7.08062 1.16454C7.10572 1.22599 7.11842 1.29183 7.11782 1.35822C7.11722 1.42461 7.10342 1.49022 7.07722 1.55122C7.05102 1.61222 7.01292 1.6674 6.96522 1.71352L4.31871 4.36002L6.96522 7.00648C7.05632 7.10078 7.10672 7.22708 7.10552 7.35818C7.10442 7.48928 7.05182 7.61468 6.95912 7.70738C6.86642 7.80018 6.74102 7.85268 6.60992 7.85388C6.47882 7.85498 6.35252 7.80458 6.25822 7.71348L3.61171 5.06702L0.965207 7.71348C0.870907 7.80458 0.744606 7.85498 0.613506 7.85388C0.482406 7.85268 0.357007 7.80018 0.264297 7.70738C0.171597 7.61468 0.119017 7.48928 0.117877 7.35818C0.116737 7.22708 0.167126 7.10078 0.258206 7.00648L2.90471 4.36002L0.258206 1.71352C0.164476 1.61976 0.111816 1.4926 0.111816 1.36002C0.111816 1.22744 0.164476 1.10028 0.258206 1.00652Z"
-            fill="currentColor"
-          />
-        </svg>
-      </button>
-    </div>
-    <div className="p-4">
-      <p className="text-gray-800 dark:text-gray-400">
-        Some text as placeholder. In real life you can have the elements you
-        have chosen. Like, text, images, lists, etc.
+      <>
+  {/* Card Blog */}
+  <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+    {/* Title */}
+    <div className="max-w-2xl mx-auto text-center mb-10 lg:mb-14">
+      <h2 className="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white">
+        Insights
+      </h2>
+      <p className="mt-1 text-gray-600 dark:text-gray-400">
+        Stay in the know with insights from industry experts.
       </p>
     </div>
+    {/* End Title */}
+    {/* Grid */}
+    <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6">
+      {/* Card */}
+      <a className="group rounded-xl overflow-hidden" href="#">
+        <div className="relative pt-[50%] sm:pt-[70%] rounded-xl overflow-hidden">
+          <img
+            className="w-full h-full absolute top-0 left-0 object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out rounded-xl"
+            src="https://images.unsplash.com/photo-1586232702178-f044c5f4d4b7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1035&q=80"
+            alt="Image Description"
+          />
+          <span className="absolute top-0 right-0 rounded-tr-xl rounded-bl-xl text-xs font-medium bg-gray-800 text-white py-1.5 px-3 dark:bg-gray-900">
+            Hot
+          </span>
+        </div>
+        <div className="mt-7">
+          <h3 className="text-xl font-semibold text-gray-800 group-hover:text-gray-600 dark:text-gray-200">
+            Studio by Preline
+          </h3>
+          <p className="mt-3 text-gray-800 dark:text-gray-200">
+            Produce professional, reliable streams easily leveraging Preline's
+            innovative broadcast studio
+          </p>
+          <p className="mt-5 inline-flex items-center gap-x-1.5 text-blue-600 decoration-2 group-hover:underline font-medium">
+            Read more
+            <svg
+              className="w-2.5 h-2.5"
+              width={16}
+              height={16}
+              viewBox="0 0 16 16"
+              fill="none"
+            >
+              <path
+                d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14"
+                stroke="currentColor"
+                strokeWidth={2}
+                strokeLinecap="round"
+              />
+            </svg>
+          </p>
+        </div>
+      </a>
+      {/* End Card */}
+      {/* Card */}
+      <a className="group rounded-xl overflow-hidden" href="#">
+        <div className="relative pt-[50%] sm:pt-[70%] rounded-xl overflow-hidden">
+          <img
+            className="w-full h-full absolute top-0 left-0 object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out rounded-xl"
+            src="https://images.unsplash.com/photo-1542125387-c71274d94f0a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+            alt="Image Description"
+          />
+        </div>
+        <div className="mt-7">
+          <h3 className="text-xl font-semibold text-gray-800 group-hover:text-gray-600 dark:text-gray-200">
+            Onsite
+          </h3>
+          <p className="mt-3 text-gray-800 dark:text-gray-200">
+            Optimize your in-person experience with best-in-class capabilities
+            like badge printing and lead retrieval
+          </p>
+          <p className="mt-5 inline-flex items-center gap-x-1.5 text-blue-600 decoration-2 group-hover:underline font-medium">
+            Read more
+            <svg
+              className="w-2.5 h-2.5"
+              width={16}
+              height={16}
+              viewBox="0 0 16 16"
+              fill="none"
+            >
+              <path
+                d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14"
+                stroke="currentColor"
+                strokeWidth={2}
+                strokeLinecap="round"
+              />
+            </svg>
+          </p>
+        </div>
+      </a>
+      {/* End Card */}
+      {/* Card */}
+      <a
+        className="group relative flex flex-col w-full min-h-[15rem] bg-center bg-cover rounded-xl hover:shadow-lg transition bg-[url('https://images.unsplash.com/photo-1634017839464-5c339ebe3cb4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3000&q=80')]"
+        href="#"
+      >
+        <div className="flex-auto p-4 md:p-6">
+          <h3 className="text-xl text-white/[.9] group-hover:text-white">
+            <span className="font-bold">Preline</span> Press publishes books
+            about economic and technological advancement.
+          </h3>
+        </div>
+        <div className="pt-0 p-4 md:p-6">
+          <div className="inline-flex items-center gap-2 text-sm font-medium text-white group-hover:text-white/[.7]">
+            Visit the site
+            <svg
+              className="w-2.5 h-2.5"
+              width={16}
+              height={16}
+              viewBox="0 0 16 16"
+              fill="none"
+            >
+              <path
+                d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14"
+                stroke="currentColor"
+                strokeWidth={2}
+                strokeLinecap="round"
+              />
+            </svg>
+          </div>
+        </div>
+      </a>
+      {/* End Card */}
+    </div>
+    {/* End Grid */}
+    
+    <>
+  {/* Card Blog */}
+  {/* Grid */}
+  <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-10">
+    {/* Card */}
+    <a className="group rounded-xl overflow-hidden" href="#">
+      <div className="relative pt-[50%] sm:pt-[70%] rounded-xl overflow-hidden">
+        <img
+          className="w-full h-full absolute top-0 left-0 object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out rounded-xl"
+          src="https://images.unsplash.com/photo-1586232702178-f044c5f4d4b7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1035&q=80"
+          alt="Image Description"
+        />
+        <span className="absolute top-0 right-0 rounded-tr-xl rounded-bl-xl text-xs font-medium bg-gray-800 text-white py-1.5 px-3 dark:bg-gray-900">
+          Sponsored
+        </span>
+      </div>
+      <div className="mt-7">
+        <h3 className="text-xl font-semibold text-gray-800 group-hover:text-gray-600 dark:text-gray-200">
+          Studio by Preline
+        </h3>
+        <p className="mt-3 text-gray-800 dark:text-gray-200">
+          Produce professional, reliable streams easily leveraging Preline's
+          innovative broadcast studio
+        </p>
+        <p className="mt-5 inline-flex items-center gap-x-1.5 text-blue-600 decoration-2 group-hover:underline font-medium">
+          Read more
+          <svg
+            className="w-2.5 h-2.5"
+            width={16}
+            height={16}
+            viewBox="0 0 16 16"
+            fill="none"
+          >
+            <path
+              d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14"
+              stroke="currentColor"
+              strokeWidth={2}
+              strokeLinecap="round"
+            />
+          </svg>
+        </p>
+      </div>
+    </a>
+    {/* End Card */}
+    {/* Card */}
+    <a className="group rounded-xl overflow-hidden" href="#">
+      <div className="relative pt-[50%] sm:pt-[70%] rounded-xl overflow-hidden">
+        <img
+          className="w-full h-full absolute top-0 left-0 object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out rounded-xl"
+          src="https://images.unsplash.com/photo-1542125387-c71274d94f0a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+          alt="Image Description"
+        />
+      </div>
+      <div className="mt-7">
+        <h3 className="text-xl font-semibold text-gray-800 group-hover:text-gray-600 dark:text-gray-200">
+          Onsite
+        </h3>
+        <p className="mt-3 text-gray-800 dark:text-gray-200">
+          Optimize your in-person experience with best-in-class capabilities
+          like badge printing and lead retrieval
+        </p>
+        <p className="mt-5 inline-flex items-center gap-x-1.5 text-blue-600 decoration-2 group-hover:underline font-medium">
+          Read more
+          <svg
+            className="w-2.5 h-2.5"
+            width={16}
+            height={16}
+            viewBox="0 0 16 16"
+            fill="none"
+          >
+            <path
+              d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14"
+              stroke="currentColor"
+              strokeWidth={2}
+              strokeLinecap="round"
+            />
+          </svg>
+        </p>
+      </div>
+    </a>
+    {/* End Card */}
+    {/* Card */}
+    <a
+      className="group relative flex flex-col w-full min-h-[15rem] bg-center bg-cover rounded-xl hover:shadow-lg transition bg-[url('https://images.unsplash.com/photo-1634017839464-5c339ebe3cb4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3000&q=80')]"
+      href="#"
+    >
+      <div className="flex-auto p-4 md:p-6">
+        <h3 className="text-xl text-white/[.9] group-hover:text-white">
+          <span className="font-bold">Preline</span> Press publishes books about
+          economic and technological advancement.
+        </h3>
+      </div>
+      <div className="pt-0 p-4 md:p-6">
+        <div className="inline-flex items-center gap-2 text-sm font-medium text-white group-hover:text-white/[.7]">
+          Visit the site
+          <svg
+            className="w-2.5 h-2.5"
+            width={16}
+            height={16}
+            viewBox="0 0 16 16"
+            fill="none"
+          >
+            <path
+              d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14"
+              stroke="currentColor"
+              strokeWidth={2}
+              strokeLinecap="round"
+            />
+          </svg>
+        </div>
+      </div>
+    </a>
+    {/* End Card */}
   </div>
-  {/* ========== END SECONDARY CONTENT ========== */}
+  {/* End Grid */}
+  {/* End Card Blog */}
+</>
+
+  </div>
+  {/* End Card Blog */}
+
+  
+</>
+
+
+
+    </div>
+    
+  </main>
+  {/* ========== END MAIN CONTENT ========== */}
+
+
+
+  
 </>
 
 
