115a0815baee6febc6c8170ab332f88eec1047e5
itsmeedward <86776978+itsmeedward@users.noreply.github.com>
2023-03-12 03:44:24 +0100

    2 files modified

--- src/pages/services/web/contact.jsx
+++ src/pages/services/web/contact.jsx
@@ -22,7 +22,7 @@ export default function ContactWeb(){
 
   const [messageStatus, setMessageStatus] = useState(null);
   return(
-    <>
+ <>
   {/* Hire Us */}
   <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
     {/* Grid */}
